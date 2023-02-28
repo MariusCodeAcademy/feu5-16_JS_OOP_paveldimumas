2f497b25ab3ee1e91204c2e01d382d3b5537f0fb
Marius CA <marius@example.com>
2023-02-28 10:03:49 +0200

    Person

---
+++ js/class/Person.js
@@ -0,0 +1,10 @@
+'use strict';
+console.log('Person.js file was loaded');
+
+// sukurti Asmens klase
+// tures savybes, name, surname, birthYear
+// metodas - myAge - grazins kiek siuo metu asmeniui yra metu
+
+class Person {
+  constructor() {}
+}
