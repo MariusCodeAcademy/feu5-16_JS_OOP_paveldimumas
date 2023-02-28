73f31ab924e4fd72f7ecabfd3a640fab42324e50
Marius CA <marius@example.com>
2023-02-28 10:36:08 +0200

    person

--- js/class/Person.js
+++ js/class/Person.js
@@ -6,5 +6,36 @@ console.log('Person.js file was loaded');
 // metodas - myAge - grazins kiek siuo metu asmeniui yra metu
 
 class Person {
-  constructor() {}
+  constructor(argName, argSurname, argBirthYear) {
+    this.name = argName;
+    this.surname = argSurname;
+    this.birthYear = argBirthYear;
+  }
+
+  myAge() {
+    const yearNow = new Date().getFullYear();
+    console.log('yearNow ===', yearNow);
+    const age = yearNow - this.birthYear;
+    console.log('age ===', age);
+  }
+
+  fullName() {
+    return `${this.name} ${this.surname}`;
+  }
 }
+// const p1 = new Person('James', 'Bond', 1960);
+// const p3 = new Person('Serbentautas', 'Bordiuras', 1982);
+// console.log(p3);
+// p3.myAge();
+// const p1FullName = p1.fullName();
+// console.log('p1FullName ===', p1FullName);
+
+// // Person klase kuria tokius objektus
+// const p2 = {
+//   name: 'James',
+//   surname: 'Bond',
+//   birthYear: 1960,
+//   fullName: function () {
+//     return `${this.name} ${this.surname}`;
+//   },
+// };
---
+++ js/class/Student.js
@@ -0,0 +1,2 @@
+'use strict';
+console.log('Student.js file was loaded');
