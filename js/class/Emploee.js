8dcbeecd9f74e1a1e1730b19fda4b8d1eaf9120b
Marius CA <marius@example.com>
2023-02-28 11:05:58 +0200

    emploee

---
+++ js/class/Emploee.js
@@ -0,0 +1,12 @@
+'use strict';
+console.log('Emploee.js file was loaded');
+
+// sukurti Emploee klase
+// Emploee paveldi Person
+// papildomai pridedam hourlyRate, hoursWorked inicijuojam i 0 kai sukuriam darbuotoja
+// metoda workHours() kurio pagalba pridedam valandu prie hoursWorked
+
+const w1 = new Emploee();
+
+// prideti 8 val darbo
+w1.workHours(160);
--- js/class/Student.js
+++ js/class/Student.js
@@ -8,10 +8,28 @@ class Student extends Person {
     // studento savybes
     this.uni = university;
     this.uniYear = studyYear;
+    this.mathGrades = [];
+  }
+
+  addMathGrade(mark) {
+    // validation
+    if (isNaN(Number(mark))) {
+      // mark yra neskaicius
+      console.warn('tik skaiciai please');
+      return;
+    }
+    // mark yra skaicius
+    this.mathGrades.push(mark);
   }
 }
 
 const s1 = new Student('Jane', 'Dow', 1999, 'KTU', 1);
+s1.addMathGrade(7);
+s1.addMathGrade(8);
+s1.addMathGrade(5);
+s1.addMathGrade(10);
+s1.addMathGrade('desimt');
+s1.addMathGrade(0);
 console.log('s1 ===', s1);
 console.log('s1.fullName() ===', s1.fullName());
 s1.myAge();
