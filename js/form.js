a4e269f4412fb5688e8292d2b5e36087f0f847bf
Marius CA <marius@example.com>
2023-03-01 09:49:04 +0200

    newWorkerFormHandler

--- js/form.js
+++ js/form.js
@@ -15,9 +15,28 @@ newEFormEl.addEventListener('submit', newWorkerFormHandler);
 workerTypeEl.addEventListener('change', toggleHourlyRate);
 // Functions ============================================
 
-function newWorkerFormHandler() {}
+function newWorkerFormHandler(event) {
+  event.preventDefault();
+  // console.log('js control');
+
+  const wType = workerTypeEl.value;
+  console.log('wType ===', wType);
+
+  const newEmp = {
+    firstName: fNameEl.value,
+    lastName: lNameEl.value,
+    type: wType,
+  };
+  if (wType === 'worker') {
+    newEmp.hourlyRate = hourlyRateEl.value;
+  }
+  console.log('newEmp ===', newEmp);
+  createNewEmployee(newEmp);
+}
 
 function toggleHourlyRate() {
   console.log('hide or show hourly rate');
   hourlyRateEl.classList.toggle('d-none');
 }
+
+function createNewEmployee(newEObj) {}
